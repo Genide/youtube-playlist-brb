2d1eec66e7c237b6a369742c392498141d95d28b
Genide <Genide@users.noreply.github.com>
2020-11-08 04:41:54 +0000

    deploy: 9c0a7fd57ec48cd368ffc6d2a0a1f54dec778e0b

--- precache-manifest.a0f3190bba01cfd341d236ef660b8d0d.js
+++ precache-manifest.a0f3190bba01cfd341d236ef660b8d0d.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "dbd599d6adcc7b312f168efcbe25b01e",
+    "revision": "b818712668eb76ffcaf41a2c1966e9b2",
     "url": "/youtube-playlist-brb/index.html"
   },
   {
-    "revision": "7c4800b7149682b46f4d",
+    "revision": "6ed08cc97b150624610b",
     "url": "/youtube-playlist-brb/static/css/main.86bacd6b.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/youtube-playlist-brb/static/js/2.c202bce6.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "7c4800b7149682b46f4d",
-    "url": "/youtube-playlist-brb/static/js/main.0c8178b0.chunk.js"
+    "revision": "6ed08cc97b150624610b",
+    "url": "/youtube-playlist-brb/static/js/main.60c1e014.chunk.js"
   },
   {
     "revision": "4865a01ca0b1b6c1624e",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/youtube-playlist-brb/precache-manifest.afa1c099affc461c96b4fdce4b2a3388.js"
+  "/youtube-playlist-brb/precache-manifest.a0f3190bba01cfd341d236ef660b8d0d.js"
 );
 
 self.addEventListener('message', (event) => {
